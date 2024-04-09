6a790a6ee2b9f911bda88cd8e69a26efef1556d3
Shubhanshu002 <75690298+Shubhanshu002@users.noreply.github.com>
2024-04-09 15:32:17 +0530

    Segregate_0;s_and_1's.c

---
+++ Segregate_0;s_and_1's.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int a[20],n,i,j,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]!=0 && a[j]==0)
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+    return 0;
+}
